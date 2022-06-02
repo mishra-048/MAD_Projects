ba1b700d6129e6b3d4ecb206302e0dbdddc79d3e
mishra-048 <104753043+mishra-048@users.noreply.github.com>
2022-06-03 00:04:08 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/tic_tac_toe_048/MainActivity.java
+++ app/src/main/java/com/example/tic_tac_toe_048/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     boolean isWinner=false;
     int imageClicked=-1;
     int player=1;//player 1 is cross
+//     Declaring the winning state
     int [][]winningStates={{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
     int []gameState={-1,-1,-1,-1,-1,-1,-1,-1,-1};
     
